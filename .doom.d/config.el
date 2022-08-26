6789090e66ac89fb3d874f15a583c5e9626a0870
anon <thefossenjoyer@proton.me>
2022-08-26 13:27:22 +0200

    Update.

---
+++ .doom.d/config.el
@@ -0,0 +1,199 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+
+;; Fonts ;;
+
+;; (setq doom-font (font-spec :family "iosevka" :size 15)
+;;       doom-variable-pitch-font (font-spec :family "iosevka" :size 14))
+(setq doom-font (font-spec :family "Monoid" :size 15)
+       doom-variable-pitch-font (font-spec :family "Monoid" :size 14))
+
+;; END OF FONTS ;;
+
+
+;; Modus theme ;;
+
+;; Modeline ;;
+(setq modus-themes-mode-line '(borderless accented))
+
+;; Region
+(setq modus-themes-region '(bg-only no-extend))
+
+;; Syntax higlighting
+(setq modus-themes-italic-constructs t) ;; italics
+
+(setq modus-themes-paren-match '(bold intense underline))
+
+(setq modus-themes-syntax '(faint))
+
+(load-theme 'modus-vivendi t)
+;; END OF MODUS ;;
+
+;; theme
+;;(setq doom-theme 'doom-horizon)
+;;(setq doom-theme 'doom-spacegrey)
+;;(setq doom-theme 'doom-palenight)
+;;(setq doom-theme 'palenight)
+
+(add-to-list 'custom-theme-load-path "/home/anon/.emacs.d/themes")
+
+;; dashboard
+(require 'dashboard)
+(dashboard-setup-startup-hook)
+(setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
+
+;; tittle
+(setq dashboard-banner-logo-title "You will use Emacs and you will love it!")
+
+(setq dashboard-startup-banner 'logo)
+;;(setq dashboard-startup-banner "/home/anon/.doom.d/doom-emacs.png")
+
+(setq dashboard-set-heading-icons t)
+(setq dashboard-set-file-icons t)
+
+;(setq dashboard-set-navigator t)
+
+;; center widgets
+(setq dashboard-center-content nil)
+
+(setq dashboard-set-init-info t)
+(setq dashboard-init-info "There is no system but GNU and Linux is one of its kernels.")
+
+;; footnote message
+(setq dashboard-footer-messages '("Just like Emacs is better than Vim, so is Linux better than Windows."))
+(setq dashboard-footer-icon (all-the-icons-octicon "dashboard"
+                                                   :height 1.1
+                                                   :v-adjust -0.05
+                                                   :face 'font-lock-keyword-face))
+(setq dashboard-items '((recents  . 5)
+                        (bookmarks . 5)
+                        (projects . 5)))
+
+(setq doom-fallback-buffer-name "*dashboard*")
+
+;; Beacon
+(beacon-mode 1)
+
+;; [] issue
+;; (global-set-key "\M-8" (lambda () (interactive) (insert "[")))
+;; (global-set-key "\M-9" (lambda () (interactive) (insert "]")))
+
+;; Treemacs ;;
+(map! :leader
+      :desc "Launch Treemacs"
+      "?" #'treemacs
+      )
+;; END OF TREEMACS ::
+
+;; CL && Roswell ;;
+(setq inferior-lisp-program "ros -Q run")
+;; END OF CL ;;
+
+;; elcord, cuz why not ;;
+(setq elcord-editor-icon "emacs_icon")
+(setq elcord-use-major-mode-as-main-icon t)
+;; END OF ELCORD ::
+
+
+;(spaceline-spacemacs-theme)
+
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+
+;; EXWM ;;
+
+;; (require 'exwm)
+;; (require 'exwm-config)
+;; (exwm-config-default)
+;; (require 'exwm-randr)
+
+;; (setq exwm-randr-workspace-monitor-plist '(0 "HDMI-0"))
+;; (add-hook 'exwm-randr-screen-change-hook
+;;           (lambda ()
+;;             (start-process-shell-command
+;;              "xrandr" nil "xrandr --output HDMI-0 --mode 1440x900 --pos 0x0 --rotate-normal")))
+
+;; (exwm-randr-enable)
+
+;; (require 'exwm-systemtray)
+;; (exwm-systemtray-enable)
+
+;; END ;;
+
+
+;; LSP ;;
+
+;; gopls ;;
+;; (lsp-register-custom-settings
+;;  '(("gopls.completeUnimported" t t)
+;;    ("gopls.staticcheck" t t)))
+;; ;; end of gopls ;;
---
+++ .doom.d/custom.el
@@ -0,0 +1,55 @@
+(put 'customize-themes 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#232530" "#e95678" "#09f7a0" "#fab795" "#21bfc2" "#6c6f93" "#59e3e3" "#c7c9cb"])
+ '(custom-safe-themes
+   '("e87f48ec4aebdca07bb865b90088eb28ae4b286ee8473aadb39213d361d0c45f" "84b14a0a41bb2728568d40c545280dbe7d6891221e7fbe7c2b1c54a3f5959289" "47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "32ebe93f59380e6173100d8ac2f17c1633e4d50b3d9d3b227579edcb5a58ba5b" "f62d44e5cef04b4bed400648b20b5b6bf7e7a9c951aaee6653d38d5278770733" "a0be7a38e2de974d1598cf247f607d5c1841dbcef1ccd97cded8bea95a7c7639" default))
+ '(doom-modeline-mode t)
+ '(elcord-display-buffer-details t)
+ '(exwm-floating-border-color "#16161c")
+ '(fci-rule-color "#f9cec3")
+ '(highlight-tail-colors ((("#203a3b") . 0) (("#283841") . 20)))
+ '(ispell-dictionary nil)
+ '(jdee-db-active-breakpoint-face-colors (cons "#16161c" "#e95678"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#16161c" "#09f7a0"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#16161c" "#6a6a6a"))
+ '(linum-format " %3i ")
+ '(objed-cursor-color "#e95678")
+ '(package-selected-packages
+   '(ujelly-theme nano-theme twilight-anti-bright-theme faff-theme humanoid-themes kaolin-themes twilight-theme tao-theme sly-quicklisp slime material-theme page-break-lines inkpot-theme exotica-theme ample-theme exwm spaceline-all-the-icons spaceline modus-operandi-theme modus-themes cmake-mode spacemacs-theme spacegray-theme phoenix-dark-pink-theme phoenix-dark-mono-theme monokai-pro-theme dashboard))
+ '(pdf-view-midnight-colors (cons "#c7c9cb" "#232530"))
+ '(rustic-ansi-faces
+   ["#232530" "#e95678" "#09f7a0" "#fab795" "#21bfc2" "#6c6f93" "#59e3e3" "#c7c9cb"])
+ '(vc-annotate-background "#232530")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#09f7a0")
+    (cons 40 "#59e19c")
+    (cons 60 "#a9cc98")
+    (cons 80 "#fab795")
+    (cons 100 "#f6ab8f")
+    (cons 120 "#f39f89")
+    (cons 140 "#f09383")
+    (cons 160 "#c48788")
+    (cons 180 "#987a8d")
+    (cons 200 "#6c6f93")
+    (cons 220 "#95668a")
+    (cons 240 "#bf5e81")
+    (cons 260 "#e95678")
+    (cons 280 "#c95b74")
+    (cons 300 "#a96071")
+    (cons 320 "#89656d")
+    (cons 340 "#f9cec3")
+    (cons 360 "#f9cec3")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-group 'disabled nil)
